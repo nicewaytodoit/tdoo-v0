ef97976d153af5aaee5118b15b5805a6b5c4d2ab
Aleksandar Ristevski <nice@ristevski.me>
2018-09-19 11:02:52 +0100

    Header title change to Tdoo's

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const header = (props) => {
     
     return (
         <header className="header">
-            <h1>todos <strong style={{fontSize: '0.2em'}}>v0.1</strong></h1>
+            <h1>tdoo's <strong style={{fontSize: '0.2em'}}>v0.1</strong></h1>
         </header>
     )
 }
